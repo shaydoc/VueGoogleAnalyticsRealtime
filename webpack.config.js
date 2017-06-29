0664afe22864ab9b081527b6f12c63e90075698a
Shay Doherty <seamusd@stiona.com>
2017-06-29 16:22:59 +0100

    updates to gutter

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.[hash:6].js'
+    filename: 'build.js'
   },
   module: {
     rules: [
